fe05a261e37fef6c2e50b59343a581f34bab2036
Carlos González de Cosío Torres <carlosgdct@gmail.com>
2021-12-02 19:11:54 -0600

    Initial commit

---
+++ Assets/Scripts/Script_3.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_3 : MonoBehaviour
+{
+    public GameObject PrefabCapsule;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       
+    }
+    private void OnEnable()
+    {
+        GameObject tempGameObject = Instantiate<GameObject>(PrefabCapsule);
+        Color a = new Color(Random.value, Random.value, Random.value);
+        tempGameObject.GetComponent<MeshRenderer>().material.color = a;
+        tempGameObject.transform.position = new Vector3(Random.Range(4, 5), Random.Range(4, 5), Random.Range(4, 5));
+    }
+    private void OnDisable()
+    {
+        GameObject tempGameObject = Instantiate<GameObject>(PrefabCapsule);
+        Color b = new Color(Random.value, Random.value, Random.value);
+        tempGameObject.GetComponent<MeshRenderer>().material.color = b;
+        tempGameObject.transform.position = new Vector3(Random.Range(-2, 2), Random.Range(-3, 3), Random.Range(-1, -1));
+    }
+}
+//cambiar tu primitiva 3 de color aleatoriamente en la funcion FixedUpdate; MeshRenderer().Material.color 
+//crear un cubos en las funciones OnDisable() y OnEnable() Instantiate<>
